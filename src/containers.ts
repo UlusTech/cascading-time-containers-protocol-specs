dc704d3227c93e79e059aafa46351c68978fb1af
Whoam <w.who.am@proton.me>
2026-09-06 04:40:29 +0300

    cascades are just containers, containers are everything
    Update on container logic

--- src/containers.ts
+++ src/containers.ts
@@ -1,129 +1,140 @@
 /**
- * Time containers
+ * Time containers.
+ *
+ * {@linkcode Container}s holds containers in them, so they create cascades.
+ * Every container insade that container has an pos, so they push each other.
+ * Order is time. X comes before Y, and Z is right next of the Y. So X is before for Y, Z is after.
+ *
+ * If you want to create the classical calendar system we have, you need to invent it seperately.
+ * Containers are there for the Freely express anything. There is no direct relations between containers and celandar blocks.
+ *
+ * If you want to create an calendar system, you create an plugin that adds meta and engine data to containers;
+ * than you add those containers insade of your own custom codded (Custom UI and working logic) containers. Those behave in the way you want.
+ * So you design an UI that you can see the containers in a classical time system, and move them insade the dates and stuff.
+ * You can not create every minute on every year, so you cheat a bit; you dont make every year real. You make one year and use it multiple times.
+ * The ones that store data will store it.
+ *
  * @module
  */
+/** biome-ignore-all lint/correctness/noUnusedVariables: <We are working on un-used data to design how system works.> */
 
 import type { Brand } from "ts-brand";
-
-export type CascadeID = Brand<string, "CascadeID">;
-
-export type ContainerID = Brand<string, "ContainerID">;
-
-type PosOnCascade = {
-	start: { containerID: ContainerID; pos: number }[];
-	end: { containerID: ContainerID; pos: number }[];
-};
-
-type ContainerSize =
-	| {
-			kind: "fixed";
-			value: number;
-	  }
-	| {
-			kind: "subTotal";
-	  };
-
-export type BaseContainer = {
-	id: ContainerID;
-};
-
-export type Container = BaseContainer & {
-	name: string;
-	cascadeIDs: CascadeID[];
-};
-
-export type ContainerInCascade = BaseContainer & {
-	size: ContainerSize;
-	childrenIds: ContainerID[];
-	cascadeID: CascadeID;
-	pos: PosOnCascade;
-};
-
-// [key in CascadeID]: PosOnCascade
-
-const AnCascadeID: CascadeID = "cascadeID" as CascadeID;
-const _OtherCascadeID: CascadeID = "otherCascadeID" as CascadeID;
-
-export const AnContainer: ContainerInCascade = {
-	id: "xyz" as ContainerID,
-	size: {
-		kind: "fixed",
-		value: 10,
-	},
-	childrenIds: [],
-	cascadeID: AnCascadeID,
-	pos: {
-		start: [{ containerID: "xyz" as ContainerID, pos: 0 }],
-		end: [{ containerID: "xyz" as ContainerID, pos: 1 }],
-	},
-};
-
-export type StaticContainer = {
-	kind: "static";
-	size: number;
-	pos: {
-		start: object;
-		end: number;
-	};
-	// parent: Container;
-};
+import type {
+	PluginContainerEngineData,
+	PluginContainerMetaData,
+	PluginID,
+} from "./plugin";
 
 /**
- * ms has 1000 steps before becoming {@linkcode SecondsContainer}
+ * Every container has its own uniqe ID.
+ */
+type ContainerID = Brand<string, "ContainerID">;
+
+/**
+ * An container is actually an box, that can have boxes insade of it; and be in boxes.
  *
- * It actually does not becomes an second, but it gets to its new ms.
- * Like: "starts at 0, ends at 700" to "starts at 0:0 ends at 1:200", this means its in the first seconds 200th ms (idk if i said it right)
+ * The "box" you see in most calendars can not host other ones, to you have single depth.
+ * This is fixed by making containers be able to contain others insade of it. See {@linkcode parents} and {@linkcode childirens}.
+ *
+ * An container might be in multiple containers, and their order only depends on each parent container.
+ * So one container can be insade multiple, and that does not (necessarily) bothers others. See {@linkcode childirens}.
+ *
+ * An container being insade of an other container does not means it needs an position on that containers parent.
+ * So "Big talk" can be insade the "Dinner event" but it does not needs to be a childiren of the time containers.
+ *
+ * Order is "when", and it does not needs to correspond to a spesific container, like a day, hour, minute or second.
+ * They can, if you want them to.
+ *
+ * Containers are arragend by an engine that checks for conditions, moves with conditions, predicts and plans.
+ * This way, you always have an up-to-date calendar, with no conflicts.
+ *
+ * TODO: We need to think about the "path" things.
+ * We have a logic called "path", it makes you be able to get a spesific container on reqursive systems.
+ * Like "gregorian-year/2026/gregorian-month/09/gregorian-day/06/utc-hour/16/utc-minute/30/dinner-event" is the event at 06-09-2026 16:30
+ * But there can be an other dinner event that is at 2027 or 2025.
+ * You should be able to generative/reqursive container logic.
+ * Single container at multiple dates, and does not crosses each other. Like when one gets delayed, other ones stay same.
+ * Think about this. This is important.
  */
-export type MiliSecondsContainer = StaticContainer & {
-	size: 1000;
+type Container = {
+	/**
+	 * The uniqe identifier of this container
+	 */
+	id: ContainerID;
+	/**
+	 * Every container stores where it is on its parent.
+	 *
+	 * TODO: Question if we should do {@linkcode parents} reverse. Insted of parents hosting the pos, childiren holding it.
+	 */
+	parents: ContainerID[];
+	/**
+	 * Every container has its childiren listed.
+	 * Currently, you get a containers pos by getting that parents chiliren list.
+	 * This is in a better way for simulations, data sync is important.
+	 */
+	childirens: { [key in ContainerID]: number };
 };
 
 /**
- * {@linkcode MunitesContainer}
+ * The meta data helps UI to be better.
+ * Engine or plugins does not uses this meta data, its for UI and stuff.
  */
-export type SecondsContainer = StaticContainer & {
-	size: 60;
-};
-
-/**
- * {@linkcode HoursContainer}
- */
-export type MunitesContainer = StaticContainer & {
-	size: 60;
-};
-
-/**
- * {@linkcode MonthContainer}
- */
-export type HoursContainer = StaticContainer & {
-	size: 24;
-};
-
-/**
- * {@linkcode YearContainer}
- */
-export type MonthContainer = StaticContainer & {
-	size: 12;
-};
-
-/** */
-export type YearContainer = StaticContainer;
-
-export type DynamicContainer = {
-	kind: "dynamic";
-	size: number;
-	pos: {
-		start: number;
-		end: number;
+type ContainerMetaData = {
+	/**
+	 * The human readable name of it.
+	 * Usually, a better looking version of id.
+	 */
+	name: string;
+	/**
+	 * For hover info and etc
+	 */
+	details: string;
+	/**
+	 * Usually UI falls back to {@linkcode icon} if not provided
+	 */
+	image: URL;
+	/**
+	 * Usually UI falls back to {@linkcode image} if not provided
+	 */
+	icon: URL;
+	plugins: {
+		[key in PluginID]: PluginContainerMetaData;
 	};
-	// parent: Container;
 };
-
-// export type Container = StaticContainer | DynamicContainer;
 
 /**
- * Cascades contains containers
+ * Record might not be the right thing, but this is the shape when its stored.
+ * See {@linkcode ContainerMetaData}.
  */
-export type Cascade = {
-	containers: string;
+type ContainerMetaDataRecord = ContainerMetaData & { id: ContainerID };
+
+/**
+ * {@linkcode Container} and its meta data ({@linkcode ContainerMetaData}) together.
+ */
+type ContainerWithMetaData = Container & ContainerMetaData;
+
+/**
+ * Engine data is there for to help the runtime (idk should we call it engine or runtime, TODO: Think about wording.) run.
+ *
+ * It has information for moving and stuff.
+ * The formulas to move, the conditions and stuff.
+ *
+ * TODO: Write this part when working on engine
+ * // Maybe move these to an seperate file? idk.
+ */
+type ContainerEngineData = {
+	plugins: {
+		[key in PluginID]: PluginContainerEngineData;
+	};
 };
+
+/**
+ * Record might not be the right thing, but this is the shape when its stored.
+ * See {@linkcode ContainerEngineData}.
+ */
+type ContainerEngineDataRecord = ContainerEngineData & { id: ContainerID };
+
+/**
+ * {@linkcode Container} and its engine data ({@linkcode ContainerEngineData}) together.
+ */
+type ContainerWithEnginData = Container & ContainerEngineData;
